8b739c792ce158f892038f8967dcd3b93ea932f5
dikaariefs <55608970+dikaariefs23@users.noreply.github.com>
2025-10-26 15:42:18 +0000

    Pindah dan timpa backend, frontend ke dalam diks-store

--- diks-store/backend/middleware/authMiddleware.js
+++
@@ -1,6 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-module.exports = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) return res.status(401).json({ error: 'No token provided' });
-    const
